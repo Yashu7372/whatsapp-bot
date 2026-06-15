9cb63b18b5bea011b821b63a74ef841ecc1a1799
Yashu7372 <mandavilliy@gmail.com>
2026-06-15 15:06:53 +0400

    fix: restore knowledge document entity

--- src/main/java/com/whatsappbot/domain/knowledge/KnowledgeDocument.java
+++ src/main/java/com/whatsappbot/domain/knowledge/KnowledgeDocument.java
@@ -1,7 +1,18 @@
 package com.whatsappbot.domain.knowledge;
 
 import com.whatsappbot.domain.tenant.TenantEntity;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,4 +24,3 @@ import java.util.UUID;
 
 @Entity
 @Table(name = "knowledge_documents")
-@Getter
