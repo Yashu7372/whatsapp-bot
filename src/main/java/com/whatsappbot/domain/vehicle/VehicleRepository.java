cd7d24238110227eafc92a4fe2b34b2a6de7044a
Yashu7372 <mandavilliy@gmail.com>
2026-06-16 12:30:05 +0400

    Add vehicle repository

---
+++ src/main/java/com/whatsappbot/domain/vehicle/VehicleRepository.java
@@ -0,0 +1,18 @@
+package com.whatsappbot.domain.vehicle;
+
+import com.whatsappbot.domain.contact.ContactEntity;
+import com.whatsappbot.domain.tenant.TenantEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface VehicleRepository extends JpaRepository<VehicleEntity, UUID> {
+
+    List<VehicleEntity> findByTenantAndContactAndActiveTrueOrderByCreatedAtDesc(TenantEntity tenant, ContactEntity contact);
+
+    Optional<VehicleEntity> findByTenantAndPlateNumberIgnoreCaseAndActiveTrue(TenantEntity tenant, String plateNumber);
+
+    List<VehicleEntity> findByTenantAndPlateNumberContainingIgnoreCaseAndActiveTrue(TenantEntity tenant, String plateNumber);
+}
