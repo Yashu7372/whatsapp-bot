60321d7b2acb50fbac4ee2e415b286860c1a5273
Yashu7372 <mandavilliy@gmail.com>
2026-06-16 12:12:52 +0400

    feat: add webhook message idempotency lookup

--- src/main/java/com/whatsappbot/domain/message/MessageRepository.java
+++ src/main/java/com/whatsappbot/domain/message/MessageRepository.java
@@ -1,8 +1,10 @@
 package com.whatsappbot.domain.message;
 
+import com.whatsappbot.domain.tenant.TenantEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface MessageRepository extends JpaRepository<Message, UUID> {
+    boolean existsByTenantAndWaMessageId(TenantEntity tenant, String waMessageId);
 }
