6bf11adb1dc1b9aeb8be521e34f4653d5e7fff5b
Yashu7372 <mandavilliy@gmail.com>
2026-08-07 21:37:29 +0400

    fix: import authentication principal

--- src/main/java/com/whatsappbot/projectcontrols/ProjectControlsController.java
+++ src/main/java/com/whatsappbot/projectcontrols/ProjectControlsController.java
@@ -3,6 +3,7 @@ package com.whatsappbot.projectcontrols;
 import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
