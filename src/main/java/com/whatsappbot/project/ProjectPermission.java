d19a47df468d4b4802c43df1ccbe2c41e624bc39
Yashu7372 <mandavilliy@gmail.com>
2026-08-08 11:31:09 +0400

    Define project authorization capabilities

---
+++ src/main/java/com/whatsappbot/project/ProjectPermission.java
@@ -0,0 +1,28 @@
+package com.whatsappbot.project;
+
+/** Explicit business capabilities evaluated together with tenant role, project party role and scope. */
+public enum ProjectPermission {
+    PROJECT_VIEW,
+
+    DOCUMENT_VIEW,
+    DOCUMENT_CREATE,
+    DOCUMENT_EDIT,
+    DOCUMENT_ISSUE,
+    DOCUMENT_APPROVE,
+
+    TRANSMITTAL_VIEW,
+    TRANSMITTAL_CREATE,
+    TRANSMITTAL_ISSUE,
+    TRANSMITTAL_ACKNOWLEDGE,
+
+    COMMERCIAL_VIEW_PROJECT,
+    COMMERCIAL_VIEW_ORGANIZATION,
+    BUDGET_EDIT_PROJECT,
+    FORECAST_SUBMIT_ORGANIZATION,
+
+    PAYMENT_VIEW_PROJECT,
+    PAYMENT_VIEW_ORGANIZATION,
+    PAYMENT_CREATE_ORGANIZATION,
+    PAYMENT_CERTIFY,
+    PAYMENT_MARK_PAID
+}
